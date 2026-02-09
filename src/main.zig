bbae0f1dbb4b0b7e8b22ad8196c0430e830d55de
theafky <102458483+TheAfky@users.noreply.github.com>
2026-02-09 18:47:45 +0100

    Added scaling launch argument

--- src/main.zig
+++ src/main.zig
@@ -5,7 +5,20 @@ pub fn main() !void {
     var gpa = std.heap.DebugAllocator(.{}){};
     defer _ = gpa.deinit();
     const allocator = gpa.allocator();
-    var app = try App.init(allocator, 1.5);
+
+    var scaling: f32 = 1;
+
+    const args = try std.process.argsAlloc(allocator);
+    defer std.process.argsFree(allocator, args);
+
+    for (args[1..]) |arg| {
+        if (std.mem.startsWith(u8, arg, "--scaling=")) {
+            const scale_value = arg[10..];
+            scaling = std.fmt.parseFloat(f32, scale_value) catch scaling;
+        }
+    }
+
+    var app = try App.init(allocator, scaling);
     defer app.deinit();
     try app.run();
 }
